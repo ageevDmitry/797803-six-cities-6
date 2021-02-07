4fb136d0e547dbfc6f704cb8d487e5e979fd771f
Dmity <Ageev_Dmity_2016@mail.ru>
2021-02-07 16:59:45 +0300

    Добавляет готовое задание

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// Проверка git
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app";
+
+const Settings = {
+  OFFERS_COUNT: 5
+};
+
+ReactDOM.render(
+    <App
+      offersCount={Settings.OFFERS_COUNT}
+    />,
+    document.querySelector(`#root`)
+);
+
